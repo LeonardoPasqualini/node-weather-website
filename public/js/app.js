ccd65bf17f27e6d2540c15d1259d7f008e7d1bae
LeonardoPasqualini <lpleopasqualini@gmail.com>
2020-11-16 17:06:24 +0000

    Remove unnsecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Cliente side Javascrip file is loaded!')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
